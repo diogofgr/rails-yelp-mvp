9dd3935e881f64b9fa67398242aa541adbf196c5
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-02-15 13:02:24 +0000

    Fix validation of rating in review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,22 @@
 class Restaurant < ApplicationRecord
+
+  restaurant_categories = [
+    "chinese",
+    "italian",
+    "japanese",
+    "french",
+    "belgian"
+  ]
+
   has_many :reviews, dependent: :destroy
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: restaurant_categories, allow_nil: false }
+
 end
+
+
+# A restaurant must have at least a name and an address.
+# The restaurant category should belong to a fixed list
+# ["chinese", "italian", "japanese", "french", "belgian"]
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, presence: true
+
+  validates_numericality_of :rating, only_integer: true
+  validates :rating, inclusion: { in: (0..5).to_a, allow_nil: false }
+
 end
+
+# A review must have a parent restaurant.
+# A review must have a content and a rating.
+# The rating should be a number between 0 and 5.
