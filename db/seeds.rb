f9302e8934e3d52956a4ad4439b26e1b6f59177a
Diogo Farias Rodrigues <diogofgr@gmail.com>
2017-02-15 12:20:26 +0000

    Add controllers, views and models for Restaurants and Revies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "+351-212-253-423",
+    category:     "french"
+  },
+  {
+    name:         "Xiao Tie Tie",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "+351-212-253-111",
+    category:     "chinese"
+  },
+  {
+    name:         "Fruit Ninja",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "+351-212-253-757",
+    category:     "italian"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
